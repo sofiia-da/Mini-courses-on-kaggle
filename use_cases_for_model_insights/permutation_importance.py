f5a7ab92174c52611eddf4323910d64c044a5b78
Sophia Demenko <mac@Macs-MacBook-Air.local>
2026-05-07 15:32:48 +0300

    Started new course about using cases of models. Learned permutation importance

---
+++ use_cases_for_model_insights/permutation_importance.py
@@ -0,0 +1,88 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+
+data = pd.read_csv('FIFA 2018 Statistics.csv')
+y = (data['Man of the Match'] == "Yes")  # Convert from string "Yes"/"No" to binary
+feature_names = [i for i in data.columns if data[i].dtype in [np.int64]]
+X = data[feature_names]
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
+my_model = RandomForestClassifier(n_estimators=100,
+                                  random_state=0).fit(train_X, train_y)
+
+
+import eli5
+from eli5.sklearn import PermutationImportance
+
+perm = PermutationImportance(my_model, random_state=1).fit(val_X, val_y)
+eli5.show_weights(perm, feature_names = val_X.columns.tolist())
+
+
+
+
+### === EXERCISE ===
+
+# Loading data, dividing, modeling and EDA below
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
+data = pd.read_csv('train.csv', nrows=50000)
+
+# Remove data with extreme outlier coordinates or negative fares
+data = data.query('pickup_latitude > 40.7 and pickup_latitude < 40.8 and ' +
+                  'dropoff_latitude > 40.7 and dropoff_latitude < 40.8 and ' +
+                  'pickup_longitude > -74 and pickup_longitude < -73.9 and ' +
+                  'dropoff_longitude > -74 and dropoff_longitude < -73.9 and ' +
+                  'fare_amount > 0'
+                  )
+
+y = data.fare_amount
+
+base_features = ['pickup_longitude',
+                 'pickup_latitude',
+                 'dropoff_longitude',
+                 'dropoff_latitude',
+                 'passenger_count']
+
+X = data[base_features]
+
+
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
+first_model = RandomForestRegressor(n_estimators=50, random_state=1).fit(train_X, train_y)
+
+print("Data sample:")
+data.head()
+
+import eli5
+from eli5.sklearn import PermutationImportance
+
+perm = PermutationImportance(first_model, random_state=1).fit(val_X, val_y)
+eli5.show_weights(perm, feature_names = base_features)
+
+
+
+
+# create new features
+data['abs_lon_change'] = abs(data.dropoff_longitude - data.pickup_longitude)
+data['abs_lat_change'] = abs(data.dropoff_latitude - data.pickup_latitude)
+
+features_2  = ['pickup_longitude',
+               'pickup_latitude',
+               'dropoff_longitude',
+               'dropoff_latitude',
+               'abs_lat_change',
+               'abs_lon_change']
+
+X = data[features_2]
+new_train_X, new_val_X, new_train_y, new_val_y = train_test_split(X, y, random_state=1)
+second_model = RandomForestRegressor(n_estimators=30, random_state=1).fit(new_train_X, new_train_y)
+
+# Create a PermutationImportance object on second_model and fit it to new_val_X and new_val_y
+# Use a random_state of 1 for reproducible results that match the expected solution.
+perm2 = PermutationImportance(second_model, random_state=1).fit(new_val_X, new_val_y)
+
+# show the weights for the permutation importance you just calculated
+eli5.show_weights(perm2, feature_names = features_2)
