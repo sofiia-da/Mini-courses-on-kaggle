1fec0de2d6afaa15060844677adcf3e9624e316d
Sophia Demenko <mac@Macs-MacBook-Air.local>
2026-05-07 16:55:48 +0300

    Learned about pipelines and cross validation

---
+++ ml_intermediate/cross_validation.py
@@ -0,0 +1,97 @@
+import pandas as pd
+
+# Read the data
+data = pd.read_csv('../intro_to_ml/melb_data.csv')
+
+# Select subset of predictors
+cols_to_use = ['Rooms', 'Distance', 'Landsize', 'BuildingArea', 'YearBuilt']
+X = data[cols_to_use]
+
+# Select target
+y = data.Price
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+
+my_pipeline = Pipeline(steps=[('preprocessor', SimpleImputer()),
+                              ('model', RandomForestRegressor(n_estimators=50,
+                                                              random_state=0))
+                             ])
+
+from sklearn.model_selection import cross_val_score
+
+# Multiply by -1 since sklearn calculates *negative* MAE
+scores = -1 * cross_val_score(my_pipeline, X, y,
+                              cv=5,
+                              scoring='neg_mean_absolute_error')
+
+print("MAE scores:\n", scores)
+
+print("Average MAE score (across experiments):")
+print(scores.mean())
+
+
+#### ==== EXERCISES ====
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+# Read the data
+train_data = pd.read_csv('train.csv', index_col='Id')
+test_data = pd.read_csv('test.csv', index_col='Id')
+
+# Remove rows with missing target, separate target from predictors
+train_data.dropna(axis=0, subset=['SalePrice'], inplace=True)
+y = train_data.SalePrice
+train_data.drop(['SalePrice'], axis=1, inplace=True)
+
+# Select numeric columns only
+numeric_cols = [cname for cname in train_data.columns if train_data[cname].dtype in ['int64', 'float64']]
+X = train_data[numeric_cols].copy()
+X_test = test_data[numeric_cols].copy()
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+
+my_pipeline = Pipeline(steps=[
+    ('preprocessor', SimpleImputer()),
+    ('model', RandomForestRegressor(n_estimators=50, random_state=0))
+])
+from sklearn.model_selection import cross_val_score
+
+# Multiply by -1 since sklearn calculates *negative* MAE
+scores = -1 * cross_val_score(my_pipeline, X, y,
+                              cv=5,
+                              scoring='neg_mean_absolute_error')
+
+print("Average MAE score:", scores.mean())
+
+def get_score(n_estimators):
+    """Return the average MAE over 3 CV folds of random forest model.
+
+    Keyword argument:
+    n_estimators -- the number of trees in the forest
+    """
+    my_pipeline = Pipeline(steps=[
+        ('preprocessor', SimpleImputer()),
+        ('model', RandomForestRegressor(n_estimators, random_state=0))
+    ])
+    scores = -1 * cross_val_score(my_pipeline, X, y,
+                                  cv=3,
+                                  scoring='neg_mean_absolute_error')
+    return scores.mean()
+
+results = {}
+for i in range(1,9):
+    results[50*i] = get_score(50*i)
+
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+plt.plot(list(results.keys()), list(results.values()))
+plt.show()
+
+n_estimators_best = min(results, key=results.get)
+
